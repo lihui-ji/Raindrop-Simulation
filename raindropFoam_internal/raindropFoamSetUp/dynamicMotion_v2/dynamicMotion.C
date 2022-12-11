3a6a0df5478124c6cb82b9f025c39b8813abdafa
Lihui Ji <73602829+lihui-ji@users.noreply.github.com>
2022-12-11 14:11:32 -0600

    Add files via upload

---
+++ raindropFoam_internal/raindropFoamSetUp/dynamicMotion_v2/dynamicMotion.C
@@ -0,0 +1,120 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dynamicMotion.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace solidBodyMotionFunctions
+{
+    defineTypeNameAndDebug(dynamicMotion, 0);
+    addToRunTimeSelectionTable
+    (
+        solidBodyMotionFunction,
+        dynamicMotion,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::solidBodyMotionFunctions::dynamicMotion::dynamicMotion
+(
+    const dictionary& SBMFCoeffs,
+    const Time& runTime
+)
+:
+    solidBodyMotionFunction(SBMFCoeffs, runTime)
+{
+    read(SBMFCoeffs);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::solidBodyMotionFunctions::dynamicMotion::~dynamicMotion()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::septernion
+Foam::solidBodyMotionFunctions::dynamicMotion::transformation() const
+{
+    scalar t = time_.value();
+
+    // Translation of centre of gravity with constant velocity
+    const vector displacement = velocity_*t;
+
+    quaternion R(1);
+    septernion TR(septernion(-displacement)*R);
+
+    DebugInFunction << "Time = " << t << " transformation: " << TR << endl;
+
+    return TR;
+}
+
+Foam::septernion
+Foam::solidBodyMotionFunctions::dynamicMotion::transformation(vector displacement)
+{
+	scalar t = time_.value();
+	velocity_ = displacement/t;
+    if (!ifMoveX_)
+        {
+            velocity_[0] = 0;
+        }
+    if (!ifMoveY_)
+        {
+            velocity_[1] = 0;
+        }
+    if (!ifMoveZ_)
+        {
+            velocity_[2] = 0;
+        }
+	Info << "Current mesh motion speed is " << velocity_ << endl;
+	return transformation();
+}
+
+bool Foam::solidBodyMotionFunctions::dynamicMotion::read
+(
+    const dictionary& SBMFCoeffs
+)
+{
+    solidBodyMotionFunction::read(SBMFCoeffs);
+
+    SBMFCoeffs_.lookup("initialVelocity") >> velocity_;
+    ifMoveX_ = SBMFCoeffs_.lookupOrDefault <bool> ("ifMoveX",  true);
+    ifMoveY_ = SBMFCoeffs_.lookupOrDefault <bool> ("ifMoveY",  true);
+    ifMoveZ_ = SBMFCoeffs_.lookupOrDefault <bool> ("ifMoveZ",  true);
+    return true;
+}
+
+
+// ************************************************************************* //
